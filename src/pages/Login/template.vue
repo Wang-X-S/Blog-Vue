2efc589d23197219d613129cecc1ec05fda66321
13jun <820373584@qq.com>
2021-08-24 16:18:37 +0800

    实现登录注册

--- src/pages/Login/template.vue
+++ src/pages/Login/template.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="login">
     <h4>用户名</h4>
-    <el-input placeholder="用户名" v-model="username"></el-input>
+    <input placeholder="用户名" v-model="username">
     <p class="error">当前用户名已注册</p>
     <h4>密码</h4>
-    <el-input type="password" placeholder="密码" v-model="password"></el-input>
+    <input type="password" placeholder="密码" @keyup.enter="onLogin" v-model="password" >
     <p class="error">当前用户名已注册</p>
-    <el-button size="medium" >立即登录</el-button>
+    <el-button size="medium" @click="onLogin">立即登录</el-button>
     <p class="notice">没有账号？<router-link to="/register">注册新用户</router-link></p>
   </div>
 </template>
